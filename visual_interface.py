53c4ca3cfe69a6d2e3f40827fe316b5ea720566b
PerterH <peterhuang0323@outlook.com>
2021-03-06 18:48:55 +0800

    可视化软件可加载模型

--- visual_interface.py
+++ visual_interface.py
@@ -15,6 +15,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, \
 import sys
 
 from UI.main_window import Ui_MainWindow
+from detect_visual import YOLOPredict
 
 CODE_VER = "V0.1"
 
@@ -30,7 +31,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         '''按键绑定'''
         # 输入媒体
         self.import_media_pushButton.clicked.connect(self.import_media)  # 导入
-        # self.start_predict_pushButton.clicked.connect(self.play_pause_button_click)  # 开始推理
+        self.start_predict_pushButton.clicked.connect(self.predict_button_click)  # 开始推理
         # 输出媒体
         # self.open_predict_file_pushButton.clicked.connect(self.play_pause_button_click)  # 文件中显示推理视频
         # 下方
@@ -48,13 +49,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # 播放时长, 以 input 的时长为准
         self.video_length = 0
 
+        '''加载模型'''
+        self.parameter_agnostic_nms = False
+        self.parameter_augment = False
+        self.parameter_classes = None
+        self.parameter_conf_thres = 0.4
+        self.parameter_device = ''
+        self.parameter_img_size = 640
+        self.parameter_iou_thres = 0.5
+        self.parameter_output = 'inference/output'
+        self.parameter_save_txt = False
+        self.parameter_source = './area_dangerous'
+        self.parameter_update = False
+        self.parameter_view_img = False
+        self.parameter_weights = ['./weights/helmet_head_person_m.pt']
+        self.predict_model = YOLOPredict(self.parameter_device, self.parameter_weights, self.parameter_img_size)
+
     def import_media(self):
         """
         导入媒体文件
         :return:
         """
-        self.input_player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl()[0]))  # 选取视频文件
-        self.output_player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl()[0]))  # 选取视频文件
+        self.parameter_source = QFileDialog.getOpenFileUrl()[0]
+        self.input_player.setMedia(QMediaContent(self.parameter_source))  # 选取视频文件
+        self.input_player.pause()  # 显示媒体
+        # self.output_player.setMedia(QMediaContent(QFileDialog.getOpenFileUrl()[0]))  # 选取视频文件
+
+    def predict_button_click(self):
+        self.predict_model.detect(self.parameter_output, self.parameter_source, self.parameter_view_img,
+                                  self.parameter_save_txt, self.parameter_img_size, self.parameter_augment,
+                                  self.parameter_conf_thres, self.parameter_iou_thres, self.parameter_classes,
+                                  self.parameter_agnostic_nms, self.parameter_update)
 
     def change_slide_bar(self, position):
         """
