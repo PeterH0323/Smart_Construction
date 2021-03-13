47962727dc4bfde57efb6ea19345ded74c24f905
PerterH <peterhuang0323@outlook.com>
2021-03-13 10:22:59 +0800

    实现曲线图demo

--- visual_interface.py
+++ visual_interface.py
@@ -5,6 +5,8 @@
 # @File    : visual_interface.py
 # @Software: PyCharm
 # @Brief   :
+import datetime
+import random
 import time
 from pathlib import Path
 
@@ -12,6 +14,8 @@ from GPUtil import GPUtil
 from PyQt5.QtCore import *
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtGui import QPainter, QCursor, QColor, QBrush
+from PyQt5.QtChart import QDateTimeAxis, QValueAxis, QSplineSeries, QChart, QChartView, QLineSeries, QCategoryAxis
 
 from UI.main_window import Ui_MainWindow
 from detect_visual import YOLOPredict
@@ -116,7 +120,7 @@ class PredictHandlerThread(QThread):
         self.parameter_source = ''
         self.parameter_update = False
         self.parameter_view_img = False
-        self.parameter_weights = ['./weights/helmet_head_person_m.pt']
+        self.parameter_weights = ['./weights/helmet_head_person_m.pt']  # 权重文件路径，修改这里
         self.predict_model = YOLOPredict(self.parameter_device, self.parameter_weights, self.parameter_img_size)
         self.output_predict_file = ""
         # 传入的QT插件
@@ -207,6 +211,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.output_player = QMediaPlayer()  # 媒体输出的widget
         self.output_player.setVideoOutput(self.output_video_widget)
 
+        self.chart_init()
+        self.timer_init()
+
         # 播放时长, 以 input 的时长为准
         self.video_length = 0
 
@@ -215,6 +222,128 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                                            self.predict_info_plainTextEdit,
                                                            self.predict_progressBar,
                                                            self.fps_label)
+
+    def timer_init(self):
+        # 使用QTimer，2秒触发一次，更新数据
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.drawLine)
+        self.timer.start(2000)
+
+    def chart_init(self):
+        # self.gpu_info_chart._chart = QChart(title="折线图堆叠")  # 创建折线视图
+        self.gpu_info_chart._chart = QChart()  # 创建折线视图
+        # chart._chart.setBackgroundVisible(visible=False)      # 背景色透明
+        self.gpu_info_chart._chart.setBackgroundBrush(QBrush(QColor("#FFFFFF")))  # 改变图背景色
+
+        self.series = QSplineSeries()
+        # 设置曲线名称
+        self.series.setName("实时数据")
+        # 把曲线添加到QChart的实例中
+        self.gpu_info_chart._chart.addSeries(self.series)
+        # 声明并初始化X轴，Y轴
+        self.dtaxisX = QDateTimeAxis()
+        self.vlaxisY = QValueAxis()
+        # 设置坐标轴显示范围
+        self.dtaxisX.setMin(QDateTime.currentDateTime().addSecs(-300 * 1))
+        self.dtaxisX.setMax(QDateTime.currentDateTime().addSecs(0))
+        self.vlaxisY.setMin(0)
+        self.vlaxisY.setMax(1500)
+        # 设置X轴时间样式
+        self.dtaxisX.setFormat("MM月dd hh:mm:ss")
+        # 设置坐标轴上的格点
+        self.dtaxisX.setTickCount(6)
+        self.vlaxisY.setTickCount(11)
+        # 设置坐标轴名称
+        self.dtaxisX.setTitleText("时间")
+        self.vlaxisY.setTitleText("量程")
+        # 设置网格不显示
+        self.vlaxisY.setGridLineVisible(False)
+        # 把坐标轴添加到chart中
+        self.gpu_info_chart._chart.addAxis(self.dtaxisX, Qt.AlignBottom)
+        self.gpu_info_chart._chart.addAxis(self.vlaxisY, Qt.AlignLeft)
+        # 把曲线关联到坐标轴
+        self.series.attachAxis(self.dtaxisX)
+        self.series.attachAxis(self.vlaxisY)
+
+        self.gpu_info_chart.setChart(self.gpu_info_chart._chart)
+
+    def drawLine(self):
+        # 获取当前时间
+        bjtime = QDateTime.currentDateTime()
+        # 更新X轴坐标
+        self.dtaxisX.setMin(QDateTime.currentDateTime().addSecs(-300 * 1))
+        self.dtaxisX.setMax(QDateTime.currentDateTime().addSecs(0))
+        # 当曲线上的点超出X轴的范围时，移除最早的点
+        if (self.series.count() > 149):
+            self.series.removePoints(0, self.series.count() - 149)
+        # 产生随即数
+        yint = random.randint(0, 1500)
+        # 添加数据到曲线末端
+        self.series.append(bjtime.toMSecsSinceEpoch(), yint)
+
+    # def create_series(self, dataTable, chart):
+    #     # 创建折线的函数
+    #     for i, data_list in enumerate(dataTable):  # [index,[list]]
+    #         # 创建曲线
+    #         series = QLineSeries(chart._chart)
+    #         # 设置折线名
+    #         series.setName(data_list[0])
+    #
+    #         for j, v in enumerate(data_list[1:]):
+    #             #  添加折线和对应的坐标点
+    #             series.append(j, v)
+    #
+    #         series.setPointsVisible(True)  # 显示原点
+    #         # 鼠标悬停连接事件
+    #         series.hovered.connect(self.onSeriesHoverd)
+    #         chart._chart.addSeries(series)  # 添加折线到视图窗口
+    #     return chart._chart
+    #
+    # def customAxisX(self, chart):
+    #     # 自定义x轴(均分)
+    #     chart = chart
+    #     series = chart.series()
+    #     if not series:
+    #         return
+    #     # 获取当前时间前8小时的一小时内的时间
+    #     time = []
+    #     for index in range(13):
+    #         num = 60 / 13
+    #         last_day = (datetime.datetime.now() + datetime.timedelta(hours=-8, minutes=- index * num)).strftime(
+    #             "%H:%M")
+    #         time.append(last_day)
+    #     category = list(reversed(time))
+    #
+    #     '''QValueAxis是轴的范围什么的不需要自己指定，轴上显示的label（也就是0,1,2,3这些内容）是默认的。
+    #     qt会根据你轴上的点自动设置。若你需要自定义一些内容，QCategoryAxis是比较好的，但是需要自己自定义好才可以调用。'''
+    #     axisx = QCategoryAxis(
+    #         chart, labelsPosition=QCategoryAxis.AxisLabelsPositionOnValue)
+    #
+    #     axisx.setGridLineVisible(False)  # 隐藏网格线条
+    #     axisx.setTickCount(len(category))  # 设置刻度个数
+    #     minx = chart.axisX().min()
+    #     maxx = chart.axisX().max()
+    #     tickc = chart.axisX().tickCount()
+    #     print(tickc)
+    #     if tickc < 2:
+    #         axisx.append(category[0])
+    #     else:
+    #         step = (maxx - minx) / (tickc - 1)  # tickc>=2
+    #         for i in range(0, tickc):
+    #             axisx.append(category[i], minx + i * step)
+    #             # 保存x轴值
+    #     chart.setAxisX(axisx, series[-1])
+    #
+    # def onSeriesHoverd(self, point, state):
+    #     # 鼠标悬停事件(底部x,y)
+    #     if state:
+    #         try:
+    #             name = self.sender().name()
+    #         except:
+    #             # QCursor.pos()悬停提示文字显示的位置
+    #             name = ""
+    #         QToolTip.showText(QCursor.pos(), "%s\nx: %s\ny: %s" %
+    #                           (name, point.x(), point.y()))
 
     def import_media(self):
         """
