bb1301630c0e0c2637ce92e144b2263294bfc3ae
Alieva Albina <55619407+AlievaA@users.noreply.github.com>
2022-11-15 16:48:36 +0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -900,6 +900,8 @@ def output_to_target(output, width, height):
     targets = []
     for i, o in enumerate(output):
         if o is not None:
+            if isinstance(o, torch.Tensor):
+                o = o.cpu().numpy()
             for pred in o:
                 box = pred[:4]
                 w = (box[2] - box[0]) / width
